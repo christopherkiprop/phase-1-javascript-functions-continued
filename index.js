82e15072e1bedb4f716c67b47f300217cf2bc41c
christopherkiprop <choppachepa@gmail.com>
2024-07-12 22:21:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+// Define saturdayFun function declaration with default activity
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define mondayWork function expression with default activity
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define wrapAdjective function to wrap an adjective in a highlight
+  function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    }
+  }
+  
+  // Exporting functions to be accessible in other modules
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  
